e0a49af38f487c9d84166031dfb3281510857e25
Alaa El-nakeeb <103367236+AlaaElnakeeb81536@users.noreply.github.com>
2023-02-10 21:36:07 +0200

    Update Q - Dreamoon and Stairs.cpp

--- Q - Dreamoon and Stairs.cpp
+++ Q - Dreamoon and Stairs.cpp
@@ -6,11 +6,14 @@
 #include <algorithm>
 using namespace std;
 /*
-num of stairs=10 ==moves can be 1 or 2 to cmplete 10
-the number of moves to be a multiple of an integer =2
+e.g1:
+num of stairs=10 and moves can be 1 or 2 to complete 10 stairs.
+the number of moves to be a multiple of an integer =2==>m
 with 1 step==>1 2 3 4 5 6 7 8 9 10=10 step
-with 2==> [1 2 ] [3 4] [ 5 6] [ 7 8] [ 9 10] ==> then it equal 5 step
-==> minnimum of them =5 but 5 can't be multiple of 2.then you can splir into [1 2] [3 4] [5 6] [7 8] [9] [10]=6
+with 2==> [1 2 ] [3 4] [ 5 6] [ 7 8] [ 9 10] ==> then it equal 5 step==>x
+==> minnimum of them =5 but 5 can't be multiple of 2.then you can split into [1 2] [3 4] [5 6] [7 8] [9] [10]=6 ==> you can make this by looping while(x%m!=0) {x++} while(5%2!=0){5++}
+==>then x will be 6 can be %2==0
+
 
 
 */
