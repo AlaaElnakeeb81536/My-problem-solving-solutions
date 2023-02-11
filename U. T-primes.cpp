b6435892a03892de4a1d202e87c2a94a64c4905a
Alaa El-nakeeb <103367236+AlaaElnakeeb81536@users.noreply.github.com>
2023-02-12 00:40:54 +0200

    Update U. T-primes.cpp

--- U. T-primes.cpp
+++ U. T-primes.cpp
@@ -6,34 +6,7 @@
 #include <stdio.h>
 #include <algorithm>
 using namespace std;
-/*
-num of stairs=10 ==moves can be 1 or 2 to cmplete 10
-the number of moves to be a multiple of an integer =2
-with 1 step==>1 2 3 4 5 6 7 8 9 10=10 step
-with 2==> [1 2 ] [3 4] [ 5 6] [ 7 8] [ 9 10] ==> then it equal 5 step
-==> minnimum of them =5 but 5 can't be multiple of 2.then you can splir into [1 2] [3 4] [5 6] [7 8] [9] [10]=6
 
-
-
-int fun(string str)
-{
-     int n = str.size();
-     int maxLength = 1, start = 0;
-     for (int i = 0; i < str.length(); i++) {
-        for (int j = i; j < str.length(); j++) {
-            int flag = 0;
-
-            for (int k = 0; k < (j - i + 1) / 2; k++)
-                if (str[i + k] == str[j - k])
-                    flag = 1;
-
-                         if (flag && (j - i + 1) > maxLength) {
-                start = i;
-                maxLength = j - i + 1;
-            }
-        }
-    }
-*/
 
 
 int main()
